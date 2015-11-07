9b6f026a163251505ad9298b4137d5cc1878a363
Marjun Makinano <marjunmakinano@gmail.com>
2015-11-07 07:19:29 -0800

    User Login function

--- wsgi/myproject/app/urls.py
+++ wsgi/myproject/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^register/$', views.user_add, name='user_add'),
+    url(r'^login/$', views.user_login, name='user_login'),
 ]
