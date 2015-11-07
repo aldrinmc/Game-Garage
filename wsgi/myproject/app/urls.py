857ebecace486d6926ca4470a7b4ff2dd60929dc
Aldrin M. Cadelina <aldrinmc143@gmail.com>
2015-11-08 03:36:05 +0800

    logout function

--- wsgi/myproject/app/urls.py
+++ wsgi/myproject/app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^register/$', views.user_add, name='user_add'),
     url(r'^login/$', views.user_login, name='user_login'),
+    url(r'^home/$', views.user_home, name='user_home'),
+    url(r'^logout/$', views.user_logout, name='user_logout'),
 ]
