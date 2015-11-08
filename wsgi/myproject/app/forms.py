00a38897913373d657764378bbc540f5eb9753c1
koneb71 <koneb2013@gmail.com>
2015-11-08 15:14:47 +0800

    add Game Form

--- wsgi/myproject/app/forms.py
+++ wsgi/myproject/app/forms.py
@@ -10,3 +10,28 @@ class UserForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput(),
         }
+
+class AddCategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+class AddGameForm(forms.ModelForm):
+    class Meta:
+        model = Game_info
+        fields = ['title', 'description', 'platform']
+
+class ChangePasswordForm(forms.ModelForm):
+    old_password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['old_password', 'password', 'confirm_password']
+
+        password = forms.CharField(widget=forms.PasswordInput)
+        widgets = {
+            'old_password': forms.PasswordInput(),
+            'password': forms.PasswordInput(),
+            'confirm_password': forms.PasswordInput(),
+        }
