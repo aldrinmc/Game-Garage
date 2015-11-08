74ff3f83f4bc2ebc2169d22b65c16cef50c6fc97
koneb71 <koneb2013@gmail.com>
2015-11-08 15:41:43 +0800

    Adding url

--- wsgi/myproject/app/urls.py
+++ wsgi/myproject/app/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^register/$', views.user_add, name='user_add'),
     url(r'^login/$', views.user_login, name='user_login'),
-    url(r'^home/$', views.user_home, name='user_home'),
     url(r'^logout/$', views.user_logout, name='user_logout'),
+    url(r'^home/$', views.user_home, name='user_home'),
     url(r'adminpanel/$', views.user_admin, name='user_admin'),
+    url(r'category/$', views.category, name='category'),
+    url(r'^(?P<pk>[0-9]+)/deletecategory/$', views.delete_category, name='delete_category'),
+    url(r'^gameinfo', views.gameinfo, name='gameinfo'),
+    url(r'changepassword', views.password_change, name='changepassword'),
 ]
